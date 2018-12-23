fe72be1405bc9339279202293d5c413cd3b97d26
Cherry Mae Pozon <cmpozon@gmail.com>
2018-12-23 14:14:18 +0800

    Update version1.cpp

--- version1.cpp
+++ version1.cpp
@@ -20,7 +20,9 @@ class Details{
 	
 	public:
 		void createDetails(); //function that gets info from user
-		
+		void showDetails() const;   //function to show info
+		int reacctno() const; //function to return acct number
+
 		 
 };
 
@@ -70,10 +72,21 @@ void Details::createDetails(){
 	
 }
 
+void Details::showDetails() const{
+	system("Color F6");	
+	cout << "\nProject No.: " << idno;
+	cout << "\nProject Name: ";
+	cout << name;
+	cout << "\nProject Page Size: " << pagesize;
+	cout << "\nProject Priority: " << priority;
+}
+
 
 
 
 void writeDetails(); //function to write record in file
+void displayInfo(int); //function to display project info given by user
+
 
 
 // ========================================================================== START OF MAIN =================================================================
@@ -81,13 +94,14 @@ void writeDetails(); //function to write record in file
 
 int main(){
 	intro();
-	char choose;
+	int choose;
 	int num;
+	int viewchoice;
 	
 	do{
 		system("cls");
 		system ("Color 0E");
-		intro();;
+		intro();
 
 	
 	cout << "\n";
@@ -104,16 +118,49 @@ int main(){
 		 << std::endl;
 	cout<<  "\n\n\tSelect Your Option (1-5): ";
 	cin >> choose;
-		switch(choose)
-		{
-		case '1':
-			system("cls");
+		switch(choose){
+			
+		case 1 :
 			
 			writeDetails();
-			
 			break;
 		
-		 case '9':
+			
+			
+			
+		case 2 :{
+		system("cls");
+		
+		system("Color F6");
+		
+		cout << "\n=============== VIEW PROJECTS ===============";
+		cout<<"\n\n\t1. ONE PROJECT";
+		cout<<"\n\n\t2. COMPLETED";
+		cout<<"\n\n\t3. ALL PROJECTS";
+		
+		cout<<"\n\n\tSelect Your Option (1-3): ";
+		cin >> viewchoice;
+				system("cls");
+				
+				
+		switch(viewchoice)	{
+			case 1:
+				
+			cout<<"\n\nEnter project number: "; 
+			cin >> num;
+			displayInfo(num);
+			break;
+			
+				
+		}
+			break;	
+
+			
+		}
+			
+			
+		
+		 case 5:
 		 	system ("Color 0A");
 			cout << "\t\t  ___ ___ _   _   _____ ___ ___ _  _   \n";
 			cout << "\t\t | __| __| | | | |_   _| __/ __| || | \n";
@@ -126,7 +173,7 @@ int main(){
 		}
 		cin.ignore();
 		cin.get();
-	}while(choose!='9');
+	}while(choose!='5');
 	return 0;
 }
 
@@ -143,7 +190,29 @@ void writeDetails() {
 	f.close();
 }
 
+//function to read a record from file
+void displayInfo (int disp){
+	Details details;
+	bool flag = 0;
+	ifstream f;
+	f.open("details.txt", ios::binary);
+	if(!f){
+		cout << "CANNOT OPEN FILE. Press ENTER to continue..";
+		return;
+	}
+	cout << "\n===============PROJECT DETAILS===============";
+	while (f.read(reinterpret_cast <char *> (&details), sizeof (Details))){
+		if (details.reacctno() == disp){
+			details.showDetails();
+			flag = 1;
+		}
+	}
+	f.close();
+	if (flag == 0)
+		cout << "\n\nSorry but the Account Number that you entered does not exist.";
+}
 	
-
+int Details::reacctno() const{
+	return idno;
+}
 	
-
